2d52f37233714e5bede0654ecd3abb6242ff8e46
Gabriella Cristini <126834537+CristiniGalvao@users.noreply.github.com>
2024-08-26 19:57:23 -0300

    ADD

--- ChatUnipar/script.js
+++ ChatUnipar/script.js
@@ -1,63 +1,67 @@
-var stompCliente = null;
+var stompClient = null;
 var username = null;
 
 document.getElementById("welcome-form").style.display = "block";
 
-function enterChatRoom(){
+function enterChatRoom() {
+
     username = document.getElementById("username").value.trim();
 
     if(username){
-        document.getElementById("welcome-from").style.display = "none";
+        document.getElementById("welcome-form").style.display = "none";
         document.getElementById("chat-room").style.display = "block";
         connect();
     }else{
-        alert("Por favor, insira um nickname!");
+        alert("Por favor, inserir um nickname");
     }
 }
 
-function connect() {
-    var socket = new SockJS('https://164c-177-91-39-198.ngrok-free.app/chat-websockt', {
-        headers: {
-            'ngrok-skip-browser-warning': 'true'
-        }
-    });
-    stompClient = Stomp.over(socket);
+    function connect() {
 
-    stompCliente.connect({},function(frame){
-        console.log('Conectando ....'+frame)
-
-        stompCliente.subscribe('/topic/public',function(messageOutput){
-        showMenssage(JSON.parse(menssageOutput.body));
+        var socket = new SockJS('https://9e7df17fa210.ngrok.app/chat-websocket', {
+            headers: {
+                'ngrok-skip-browser-warning': 'true'
+            }
         });
-        stompCliente.send("app/addUser", {},JSON.stringify({
-            sender: username,
-            type: 'JOIN'
-        }));
-    })
-}
+        stompClient = Stomp.over(socket);
 
-function sendMessage(){
-    var messageContent = document.getElementById("menssageInput").value.trim();
+        stompClient.connect({}, function (frame){
+            console.log('conectando...'+ frame);
 
-    if(messageContet && stompCliente){
-        var chatMessage = {
-            sender: username,
-            content: messageContent,
-            type:'CHAT'
-        };
-        stompCliente.send(/app/sendMessage)
+            stompClient.subscribe('/topic/public', function(messageOutput) {
+                showMessage(JSON.parse(messageOutput.body));
+            });
+
+            stompClient.send("/app/addUser", {}, JSON.stringify({
+                sender: username,
+                type :'JOIN'
+            }));
+        });
     }
-}
+    function showMessage(message){
+        var messageElement = document.createElement('div');
 
-function showMenssage(message){
-    var messageElement = document.createElement('div');
-
-    if(message.type == 'JOIN'){
-        menssageElement.innerText = message.sender + "entrou na sala.";
-    }else if(messageElement.type == 'LEAVE'){
-        messageElement.innerText = message.sender + "saiu da sala";
-    }else{
-        messageElement.innerText = message.sender + "disse: "+message.content;
+        if(message.type === 'JOIN'){
+            messageElement.innerText = message.sender + " entrou na sala ";
+        }else if(message.type === 'LEAVE') {
+            messageElement.innerText = message.sender + " saiu da sala ";
+        }else {
+            messageElement.innerText = message.sender + " disse: " + message.content;
+        }    
+     
+     document.getElementById('messages').appendChild(messageElement);
     }
-    document.getElementById("messages").appendChild(messageElement);
-}
+
+    function sendMessage(){
+        var messageContent = document.getElementById("messageInput").value.trim();
+
+        if(messageContent && stompClient){
+            var chatMessage = {
+                sender: username,
+                content: messageContent,
+                type: 'CHAT'
+            };
+            stompClient.send('/app/sendMessage',{},JSON.stringify(chatMessage));
+            document.getElementById("messageInput").value = '';
+        }
+    }
